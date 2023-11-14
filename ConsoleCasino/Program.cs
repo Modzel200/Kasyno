9bf9562b1525d9fcfcefb83c2be6a3efbff48f58
c0ndii <105825543+c0ndii@users.noreply.github.com>
2023-11-14 17:46:38 +0100

    Update Program.cs

--- ConsoleCasino/Program.cs
+++ ConsoleCasino/Program.cs
@@ -1,11 +1,30 @@
 ﻿using ConsoleCasino;
 using ConsoleCasino.Roulette;
 using ConsoleCasino.Slots;
+using System.Runtime.InteropServices;
 
-Account account = new Account();
-Console.WriteLine("Witaj w Royal Casino "+account.getName());
-Console.WriteLine("Twój aktualny status konta: "+account.getBalance());
-//Roulette roulette = new Roulette();
-//roulette.Game(account);
-Slots te = new Slots();
-te.Game(account);
+class Program
+{
+    [DllImport("kernel32.dll", ExactSpelling = true)]
+    private static extern IntPtr GetConsoleWindow();
+    private static IntPtr ThisConsole = GetConsoleWindow();
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    private const int HIDE = 0;
+    private const int MAXIMIZE = 3;
+    private const int MINIMIZE = 6;
+    private const int RESTORE = 9;
+    static void Main(string[] args)
+    {
+        Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+        ShowWindow(ThisConsole, MAXIMIZE);
+        Account account = new Account();
+        Console.WriteLine("Witaj w Royal Casino " + account.getName());
+        Console.WriteLine("Twój aktualny status konta: " + account.getBalance());
+        Roulette roulette = new Roulette();
+        roulette.Game(account);
+        //Slots te = new Slots();
+        //te.Game(account);
+    }
+}
+
