93f7a40fed967599bd923503c9d9bf1811b26a25
Modzel200 <49248797+Modzel200@users.noreply.github.com>
2023-11-14 12:26:52 +0100

    ruleta

--- ConsoleCasino/Program.cs
+++ ConsoleCasino/Program.cs
@@ -1,10 +1,11 @@
 ﻿using ConsoleCasino;
+using ConsoleCasino.Roulette;
 using ConsoleCasino.Slots;
 
 Account account = new Account();
 Console.WriteLine("Witaj w Royal Casino "+account.getName());
 Console.WriteLine("Twój aktualny status konta: "+account.getBalance());
-//Roulette roulette = new Roulette();
-//roulette.Game(account);
-Slots te = new Slots();
-te.Game(account, 5);
+Roulette roulette = new Roulette();
+roulette.Game(account);
+//Slots te = new Slots();
+//te.Game(account, 5);
